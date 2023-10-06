1ff9b489d975a141d862fed5905e26dc83a8ee45
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2023-10-06 11:54:12 +0200

    links to submitted issues

--- src/main/java/com/mytests/Controller1.java
+++ src/main/java/com/mytests/Controller1.java
@@ -17,7 +17,8 @@ import java.util.List;
 @Controller("/first")
 public class Controller1 {
 
-
+    // mapping method javadoc is not considered:
+    //   https://youtrack.jetbrains.com/issue/IDEA-334234
     /**
      * getTest1 method
      * @param pv1 some pathvar
@@ -28,18 +29,25 @@ public class Controller1 {
         return "test1";
     }
 
+    // single parameter in the POST methods is not processed as request body if the explicit @Body annotation is not provided
+    //   https://youtrack.jetbrains.com/issue/IDEA-334227
     @Post("/test1")
     public boolean post1(List<FirstPojo> pojoList){
         return true;
     }
+    // but this is ok:
     @Post("/test2")
     public boolean post2(@Body List<FirstPojo> pojoList){
         return true;
     }
+
+    // `@Body#value` should be considered: https://youtrack.jetbrains.com/issue/IDEA-334230
     @Post("/test3")
     public String post3(@Body("p1") Integer p1 ){
         return ":"+p1;
     }
+
+    // Multiple parameters in post method https://youtrack.jetbrains.com/issue/IDEA-334228
     @Post("/test4")
     public String post4(String arg1, int arg2, boolean arg3){
         return arg1+" "+arg2+" "+arg3;
